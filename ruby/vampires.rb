fe46428145d2bd075200c73fc71ab0eebbd181ee
kaley.laporta <kaley.laporta@gmail.com>
2017-02-17 08:32:57 -0600

    garlic_affinity and health_insurance_seeker methods defined for last two questions of survey

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -8,7 +8,25 @@ puts "What year were you born?"
 birth_year = gets.chomp
 
 puts "Our company cafeteria serves garlic bread. Should we order some for you?"
-# boolean
+garlic_lover = gets.chomp
+def garlic_affinity
+	if garlic_lover == "yes"
+		garlic_affinity is true
+	elsif garlic_lover == "no"
+		garlic_affinity is false
+	else
+		puts "Please answer yes or no."
+	end
+end
 
 puts "Would you like to enroll in the company's health insurace?"
-# boolean
+needs_insurance = gets.chomp
+def health_insurance_seeker
+	if needs_insurance == "yes"
+		health_insurance_seeker is true
+	elsif needs_insurance == "no"
+		health_insurance_seeker is false
+	else
+		puts "Please answer yes or no."
+	end
+end
