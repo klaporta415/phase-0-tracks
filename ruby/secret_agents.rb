8f93a33a28f13ab678cf32ac01d19d41fa87b43d
kaley.laporta <kaley.laporta@gmail.com>
2017-02-17 14:47:44 -0600

    methods fixed. Methods tested on various strings and password input. Agent password encrypt/decrypt question with encrypt/decrypt methods to be called based on user input.

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -1,26 +1,57 @@
-def encrypt
-#get password from user
+def encrypt(password)
+    password_length = password.length
+    index = 0
+    while index < password_length
+        password[index] = password[index].next
+        index += 1
+    end
+    p password
+end
+
+#find index of letter in alphabet
+#subtract index by 1
+#find new letter represented by index - 1
+
+def decrypt(password)
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    password_length = password.length
+    index = 0
+    while index < password_length
+        #letter in alphabet of that index number - 1(number of of that letter in the alphabet(letter_original)-1)
+        password[index] = alphabet[(alphabet.index(password[index]))-1]
+        index += 1
+    end
+    p password
+end
+
+# encrypt("hello")
+# decrypt("goodbye")
+
+# puts "Enter password"
+# password =  gets.chomp
+
+# encrypt(password)
+# decrypt(password)
+
+# encrypt("abc")
+# encrypt("zed")
+# decrypt("bcd")
+# decrypt("afe")
+# decrypt(encrypt("swordfish"))
+
+# Ask agent if they want to encrypt/decrypt password
+# Ask agent for password
+# Encrypt or decrypt password based on user input
+
+puts "Would you like to decrypt or encrypt a password?"
+password_scrambler = gets.chomp
 puts "Enter password"
-password = gets.chomp
-#advance every letter of string one letter forward
-password_length = password.length
+agent_password = gets.chomp
 
-index = 0
-
-=begin
-def next
-while index < password_length
-	p password[index].next 
-	index += 1
+if password_scrambler == "encrypt"
+    p encrypt(agent_password)
+elsif password_scrambler == "decrypt"
+    p decrypt(agent_password)
+else
+    puts "Try again!"
 end
-end
-=end
-index = 0
-while index < password_length
-	password[index] = password[index].next
-	index += 1
-end
-puts password
-end
-
-encrypt 
