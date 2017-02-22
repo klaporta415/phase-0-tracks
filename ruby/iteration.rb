682afd90e5b00925f43e1503fc85c6a72dd939be
kaley.laporta <kaley.laporta@gmail.com>
2017-02-21 18:07:15 -0600

    arrays with built-in methods to filter added from pairing session.

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -32,3 +32,39 @@ upcase_actors_movies = actors_movies.map do |actor, movie|
 end
 
 p upcase_actors_movies
+
+# array1 = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ]
+
+# p array1.delete_if { |array1| 
+# array1 < 2}
+
+
+# p array1.keep_if { |array1|
+# array1 > 2 }
+
+# p array1.select {|x|
+# x > 3 }
+
+
+array2 = [ "orange", "green", "blue", "red", "yellow", "aqua"]
+  
+ array2.each do |colors|
+  colors.length
+  good_length = 4 
+  if colors.length > good_length
+  p colors.chop
+  while good_length == false
+
+  end
+  good_length = true
+
+  end
+  p 
+end
+
+  # while good_length > 4 
+  # colors.chop
+   
+ # end
+ # end
+ # puts colors
