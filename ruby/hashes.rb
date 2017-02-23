50ea3c5b0b55629e514990816ceaa7a9788e0b09
kaley.laporta <kaley.laporta@gmail.com>
2017-02-23 13:28:27 -0600

    pseudocode added to file

---
+++ ruby/hashes.rb
@@ -0,0 +1,7 @@
+# create hash with form to fill out by designer
+# prompt designer to input each key
+# convert the string to appropriate data type
+# print the hash at the end
+# designer has ability to edit values
+# designer can finish program by entering "done"
+# print updated hash
