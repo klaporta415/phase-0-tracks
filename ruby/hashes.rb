ef51199d6a108c7dc816fb60d2939e937421eaad
kaley.laporta <kaley.laporta@gmail.com>
2017-02-23 13:31:28 -0600

    hash created to represent empty form for designer to fill out

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -1,4 +1,12 @@
 # create hash with form to fill out by designer
+client_info {
+	client_name: "",
+	client_age: "",
+	number_children: "",
+	decor_theme: "",
+	difficult: "",
+	number_pets: "",
+}
 # prompt designer to input each key
 # convert the string to appropriate data type
 # print the hash at the end
