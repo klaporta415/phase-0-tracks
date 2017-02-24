56182806ce6bebbc24571388fb6d20fda81b9e14
kaley.laporta <kaley.laporta@gmail.com>
2017-02-24 17:41:31 -0600

    all methods found and printed

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -4,36 +4,36 @@
 # When there's more than one suspect who could have
 # committed the crime, add additional calls to prove it.
 
-# p"iNvEsTiGaTiOn".swapcase
+p"iNvEsTiGaTiOn".swapcase
 # => “InVeStIgAtIoN”
 
-# p "zom".insert(1, 'o')
+p "zom".insert(1, 'o')
 # => “zoom”
 
-# p "enhance".center(15)
+p "enhance".center(15)
 # => "    enhance    "
 
-# p "Stop! You’re under arrest!".upcase
+p "Stop! You’re under arrest!".upcase
 # => "STOP! YOU’RE UNDER ARREST!"
 
-# p "the usual".insert(-1, ' suspects')
+p "the usual".insert(-1, ' suspects')
 #=> "the usual suspects"
 
-# p " suspects".insert(0, 'the usual')
+p " suspects".insert(0, 'the usual')
 # => "the usual suspects"
 
-# p"The case of the disappearing last letter".chop
+p"The case of the disappearing last letter".chop
 # => "The case of the disappearing last lette"
 
-# p "The mystery of the missing first letter".delete "T"
+p "The mystery of the missing first letter".delete "T"
 # => "he mystery of the missing first letter"
 
-# p "Elementary,    my   dear        Watson!".center(10)
+p "Elementary,    my   dear        Watson!".center(10)
 # => "Elementary, my dear Watson!"
 
-p "z".to_f
+p "z".ord
 # => 122 
 # (What is the significance of the number 122 in relation to the character z?)
 
-# "How many times does the letter 'a' appear in this string?".<???>
+p "How many times does the letter 'a' appear in this string?".count "a"
 # => 4
