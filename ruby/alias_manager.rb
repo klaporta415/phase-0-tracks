add3a5a68d7f2a995c13f0f7850f73fdd99edd82
kaley.laporta <kaley.laporta@gmail.com>
2017-02-24 11:50:26 -0600

    Ask agent for birthname, split name into array of first and last name. Name reversed using .reverse_each

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,6 +1,10 @@
 # request name from agent
+puts "Enter name, please:"
 # swaps first and last name
 	# need to .split to rearrange first/last name?
+birthname = gets.chomp
+birthname.split(' ').reverse_each { |new_name| print new_name, ' '}
+
 # change vowels to next vowel (a,e,i,o,u) and all consonants to next
 	#(bcdfghjklmnpqrstvwxyz) consonants
 	# any built in methods for vowels? consonants?
