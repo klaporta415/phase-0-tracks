5f591c97c5f1b76625825df10c6899cd801d8866
kaley.laporta <kaley.laporta@gmail.com>
2017-02-24 11:14:25 -0600

    unsuccessful print statements added to attempt to access nested date

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -10,34 +10,31 @@
 hotel = {
 	floors: {
 		second_floor: 'Second Floor',
-		rooms2: {
-			201 => [],
-			202 => ['mom', 'dad', 'son', 'daughter'],
-			203 => [],
-			204 => ['cat lady', 'orange cat', 'black cat']
-		},
+		rooms2: [
+			'201' => [],
+			'202' => ['mom', 'dad', 'son', 'daughter'],
+			'203' => [],
+			'204' => ['cat lady', 'orange cat', 'black cat']
+		],
 		third_floor: 'Third Floor',
-		rooms3: {
-			301 => ['magician', 'rabbit', 'top hat'],
-			302 => ['bank robber', 'bags of money'],
-			303 => [],
-			304 => ['sister1', 'sister2']
-		}
+		rooms3: [
+			'301' => ['magician', 'rabbit', 'top hat'],
+			'302' => ['bank robber', 'bags of money'],
+			'303' => [],
+			'304' => ['sister1', 'sister2']
+		]
 	},	
 	elevators: {
-		guest_elevators: 'Guest Elevators',
 		guest_elevators: [
 		'main lobby',
 		'back entrance',
 	],
-		service_elevators: 'Service elevators',
 		service_elevators: [
 			'utility',
 			'kitchen'
 		]
 },
 	lobby: {
-		main_lobby: 'Main Lobby',
 		concierge_desk: [
 			'concierge',
 			'bell hop',
@@ -56,3 +53,5 @@ hotel = {
 	}
 }
 p hotel
+# hotel[:lobby][:concierge_desk][1]
+p hotel[:floors][:second_floor][:'202'][2]
