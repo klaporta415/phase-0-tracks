f9d6a7894e08047de2f11db89c51907cc2a6db52
kaley.laporta <kaley.laporta@gmail.com>
2017-02-24 17:30:57 -0600

    some built-in methods entered and then commented out

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -4,34 +4,34 @@
 # When there's more than one suspect who could have
 # committed the crime, add additional calls to prove it.
 
-"iNvEsTiGaTiOn".swapcase
+# p"iNvEsTiGaTiOn".swapcase
 # => “InVeStIgAtIoN”
 
-# "zom".<???>
+# p "zom".insert(1, 'o')
 # => “zoom”
 
-# "enhance".<???>
+# p "enhance".center(15)
 # => "    enhance    "
 
-# "Stop! You’re under arrest!".<???>
+# p "Stop! You’re under arrest!".upcase
 # => "STOP! YOU’RE UNDER ARREST!"
 
-# "the usual".<???>
+# p "the usual".insert(-1, ' suspects')
 #=> "the usual suspects"
 
-# " suspects".<???>
+# p " suspects".insert(0, 'the usual')
 # => "the usual suspects"
 
-# "The case of the disappearing last letter".<???>
+# p"The case of the disappearing last letter".chop
 # => "The case of the disappearing last lette"
 
-# "The mystery of the missing first letter".<???>
+# p "The mystery of the missing first letter".delete "T"
 # => "he mystery of the missing first letter"
 
-# "Elementary,    my   dear        Watson!".<???>
+# p "Elementary,    my   dear        Watson!".center(10)
 # => "Elementary, my dear Watson!"
 
-# "z".<???>
+p "z".to_f
 # => 122 
 # (What is the significance of the number 122 in relation to the character z?)
 
