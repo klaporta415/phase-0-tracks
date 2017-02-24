c90d39461949dd33ffcd487bf325ad9756c5b556
kaley.laporta <kaley.laporta@gmail.com>
2017-02-24 11:25:53 -0600

    pseudocode written

---
+++ ruby/alias_manager.rb
@@ -0,0 +1,10 @@
+# request name from agent
+# swaps first and last name
+	# need to .split to rearrange first/last name?
+# change vowels to next vowel (a,e,i,o,u) and all consonants to next
+	#(bcdfghjklmnpqrstvwxyz) consonants
+	# any built in methods for vowels? consonants?
+	# use .next for this
+	# use .split to separate each letter
+# produce new name .join?
+# loop through name
