97e739bbb18b95d0a78a2afd9264bd04dae39de2
kaley.laporta <kaley.laporta@gmail.com>
2017-02-25 10:35:16 -0600

    vowels and consonants added as strings now.

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -6,12 +6,18 @@ birthname = gets.chomp
 p name_flip = birthname.split(' ').reverse
 # .reverse_each{ |new_name| print new_name, ' '}
 name_flip 
-vowels = ['a', 'e', 'i', 'o', 'u']
-consonants = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z']
+
+def letter_scramble(name)
+	 name.each{|name| vowels.next}
+	 name.each{|name| consonants.next}
+vowels = "aeiou"
+consonants = "bcdfghjklmnpqrstvwxyz"
+end
+p letter_scramble(name_flip)
 # change vowels to next vowel (a,e,i,o,u) and all consonants to next
 	#(bcdfghjklmnpqrstvwxyz) consonants
-	# any built in methods for vowels? consonants?
-	# use .next for this
+	# any built in methods for vowels? consonants? 
+	# use .next for this (only on strings, not arrays)
 	# use .split to separate each letter
 # produce new name .join?
 # loop through name
