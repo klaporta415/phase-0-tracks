01648e564be8eebe6207980a8f8fc4598dd8ce4b
kaley.laporta <kaley.laporta@gmail.com>
2017-02-25 11:47:18 -0600

    Fibonacci method written and test. Bubble sort method added and commented out.

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -19,12 +19,43 @@
 # p search_array(arr, 58)
 # p search_array(arr, 42)
 
-# fib = [0, 1]
 
-# fib.push(array_sum)
+def fib(n)
+	arr = [0, 1]
 
-def fib( n )
-  return  n  if (0..1).include? n
-  ( fib( n - 1 ) + fib( n - 2 ))
+	fibonacci = ->(arr) {
+		if arr.size == n
+			arr
+		else
+			current_number, last_number = arr.last(2)
+			fibonacci.(arr + [current_number + last_number])
+		end
+	}
+	fibonacci.(arr)
 end
-puts fib(5)
+p fib(6)
+p fib(100)[-1]
+
+# bubble sort
+# def bubble_sort(array)
+# 	n = array.length
+
+# 	loop do
+# 		# [1, 4, 1, 3, 4, 1, 3, 3]
+# 		swapped = false
+# 		(n-1).times do |i| #i is iterator variable
+# 			if array[i] > array [i + 1]
+# 				array[i], array[i +1] = array[i + 1], array[i]
+# 				swapped = true
+# 			end
+# 		end
+
+# 		break if not swapped
+
+# 	end
+
+# 	array
+# end
+
+# a = [1, 4, 1, 3, 4, 1, 3, 3]
+# p bubble_sort(a)
