a2774f9ff8cdf85ab0524e38cf7eb9dac04a6d57
kaley.laporta <kaley.laporta@gmail.com>
2017-02-25 10:17:48 -0600

    name reverse complete and assigned to variable name_flip. vowels and consonants arrays created.

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -3,9 +3,11 @@ puts "Enter name, please:"
 # swaps first and last name
 	# need to .split to rearrange first/last name?
 birthname = gets.chomp
-p birthname.split(' ').reverse
+p name_flip = birthname.split(' ').reverse
 # .reverse_each{ |new_name| print new_name, ' '}
-
+name_flip 
+vowels = ['a', 'e', 'i', 'o', 'u']
+consonants = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z']
 # change vowels to next vowel (a,e,i,o,u) and all consonants to next
 	#(bcdfghjklmnpqrstvwxyz) consonants
 	# any built in methods for vowels? consonants?
