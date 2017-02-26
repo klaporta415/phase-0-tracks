2a4051911e0435e4a570e626366d17397c4aa88a
kaley.laporta <kaley.laporta@gmail.com>
2017-02-26 14:32:45 -0600

    new method with letters in name going to next in the alphabet created. Able to enter multiple names and stop when 'done' entered

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,104 +1,10 @@
+# set up loop to continue changing agent names until 'done' entered
+	
 # request name from agent
-puts "Enter name, please:"
+
 # swaps first and last name
 	# need to .split to rearrange first/last name?
-birthname = gets.chomp
-name_flip = birthname.split(' ').reverse
-# .reverse_each{ |new_name| print new_name, ' '}
-name_flip 
-reverse_name = name_flip.join('')
-p agent_name = reverse_name.downcase
 
-# p agent_name
-
-# vowels = 'aeiou'
-# consonants = 'bcdfghjklmnpqrstvwxyz'
-# consonants = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z'] 
-# p agent_name.to_s
-
-# if agent_name.include? vowels
-# 	vowels.next
-# else agent_name.include? consonants
-# 	consonants.next
-# end
-
-# new_name = agent_name.split(' ')
-	
-def vowel_swap(new_name)
-	# vowels = 'aeiou'
-	# consonants = 'bcdfghjklmnpqrstvwxyz'
-	# name_length = new_name.length
-	# index = 0
-	# while index < name_length
-		new_name.gsub(/[aeiou]/,
-			'a' => 'e',
-		 	'e' => 'i',
-		 	'i' => 'o',
-		 	'o' => 'u',
-		 	'u' => 'a')
-		# return agent_name
-		# agent_name[index] = vowels[vowels.index(agent_name[index])]
-		# return agent_name
-		# index += 1
-	end
-# 	p new_name
-# end
-p vowel_swap_name = vowel_swap(agent_name)
-
-
-def consonant_swap(new_name)
-	new_name.gsub(/[bcdfghjklmnpqrstvwxyz]/, 
-				'b' => 'c',
-				'c' => 'd',
-				'd' => 'f',
-				'f' => 'g',
-				'g' => 'h',
-				'h' => 'j',
-				'j' => 'k',
-				'k' => 'l',
-				'l' => 'm',
-				'm' => 'n',
-				'n' => 'p',
-				'p' => 'q',
-				'q' => 'r',
-				'r' => 's',
-				's' => 't',
-				't' => 'v',
-				'v' => 'w',
-				'w' => 'x',
-				'x' => 'y',
-				'y' => 'z',
-				'z' => 'b')
-end
-
-p scrambled_name = consonant_swap(vowel_swap_name)
-
-# 	agent_name.map! {|agent_name| vowels.next }
-# 	agent_name
-# 	agent_name.map! {|agent_name| consonants.next }
-# name_string = name_flip.join('')
-# p agent_name
-
-# agent_name for Leslie Knope should be Lpuqi Mitmoi
-
-# def letter_scramble(name_string)
-	# vowels = "aeiou"
-# 	consonants = "bcdfghjklmnpqrstvwxyz"
-# 	name_length = name_string.length
-# 	index = 0
-# 	while index < name_length
-# 		name_string[index] = consonants[consonants.index(name_string[index])-1]
-# 		index += 1
-# 	end
-# 	p name_string
-#  end
-
-# p letter_scramble(name_string)
-
-
-# agent_name.gsub??? how can we use gsub?
-
-# p letter_scramble(name_flip)
 # change vowels to next vowel (a,e,i,o,u) and all consonants to next
 	#(bcdfghjklmnpqrstvwxyz) consonants
 	# any built in methods for vowels? consonants? 
@@ -106,3 +12,46 @@ p scrambled_name = consonant_swap(vowel_swap_name)
 	# use .split to separate each letter
 # produce new name .join?
 # loop through name
+
+    alias_calculated = false
+
+until alias_calculated
+    puts 'Agent name:'
+    birthname = gets.chomp
+
+
+# swaps first and last name
+	# need to .split to rearrange first/last name?
+
+agent_name = birthname.downcase
+
+def name_shuffle(agent_name)
+  # letter ="abcdefghijklmnopqrstuvwxyz"
+  name_length = agent_name.length
+  index = 0
+  while index < name_length
+    agent_name[index] = agent_name[index].next
+  index += 1
+  end
+  agent_name
+end
+alias_name = name_shuffle(agent_name) 
+alias_name
+name_flip = alias_name.split(' ').reverse
+name_flip 
+reverse_name = name_flip.join('')
+almost_final_name = reverse_name.gsub(/[!]/, " ")
+
+almost_final_name
+almost_final_name.upcase
+
+final_name = almost_final_name.split.map{|i| i.capitalize}.join(' ')
+p final_name
+
+   if birthname == "done" 
+      alias_calculated = true
+    else 
+      alias_calculated = false
+    end 
+
+  end
