9f86ab36563b860fffe16afd9dfe4469f02ff7ce
kaley.laporta <kaley.laporta@gmail.com>
2017-02-26 12:51:17 -0600

    vowel_swap and consonant_swap methods created using .gsub

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -5,25 +5,88 @@ puts "Enter name, please:"
 birthname = gets.chomp
 name_flip = birthname.split(' ').reverse
 # .reverse_each{ |new_name| print new_name, ' '}
-p name_flip 
+name_flip 
 reverse_name = name_flip.join('')
-agent_name = reverse_name.chars
-vowels = "aeiou"
-consonants = "bcdfghjklmnpqrstvwxyz" 
-	agent_name.map! {|agent_name| vowels.next }
-	agent_name
-	agent_name.map! {|agent_name| consonants.next }
-# name_string = name_flip.join('')
-p agent_name
+p agent_name = reverse_name.downcase
 
-# agent_name for Leslie Knope should be Louqi Mitmoi
+# p agent_name
+
+# vowels = 'aeiou'
+# consonants = 'bcdfghjklmnpqrstvwxyz'
+# consonants = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z'] 
+# p agent_name.to_s
+
+# if agent_name.include? vowels
+# 	vowels.next
+# else agent_name.include? consonants
+# 	consonants.next
+# end
+
+# new_name = agent_name.split(' ')
+	
+def vowel_swap(new_name)
+	# vowels = 'aeiou'
+	# consonants = 'bcdfghjklmnpqrstvwxyz'
+	# name_length = new_name.length
+	# index = 0
+	# while index < name_length
+		new_name.gsub(/[aeiou]/,
+			'a' => 'e',
+		 	'e' => 'i',
+		 	'i' => 'o',
+		 	'o' => 'u',
+		 	'u' => 'a')
+		# return agent_name
+		# agent_name[index] = vowels[vowels.index(agent_name[index])]
+		# return agent_name
+		# index += 1
+	end
+# 	p new_name
+# end
+p vowel_swap_name = vowel_swap(agent_name)
+
+
+def consonant_swap(new_name)
+	new_name.gsub(/[bcdfghjklmnpqrstvwxyz]/, 
+				'b' => 'c',
+				'c' => 'd',
+				'd' => 'f',
+				'f' => 'g',
+				'g' => 'h',
+				'h' => 'j',
+				'j' => 'k',
+				'k' => 'l',
+				'l' => 'm',
+				'm' => 'n',
+				'n' => 'p',
+				'p' => 'q',
+				'q' => 'r',
+				'r' => 's',
+				's' => 't',
+				't' => 'v',
+				'v' => 'w',
+				'w' => 'x',
+				'x' => 'y',
+				'y' => 'z',
+				'z' => 'b')
+end
+
+p scrambled_name = consonant_swap(vowel_swap_name)
+
+# 	agent_name.map! {|agent_name| vowels.next }
+# 	agent_name
+# 	agent_name.map! {|agent_name| consonants.next }
+# name_string = name_flip.join('')
+# p agent_name
+
+# agent_name for Leslie Knope should be Lpuqi Mitmoi
 
 # def letter_scramble(name_string)
 	# vowels = "aeiou"
 # 	consonants = "bcdfghjklmnpqrstvwxyz"
 # 	name_length = name_string.length
 # 	index = 0
-# 	while index < name_string.length
+# 	while index < name_length
 # 		name_string[index] = consonants[consonants.index(name_string[index])-1]
 # 		index += 1
 # 	end
