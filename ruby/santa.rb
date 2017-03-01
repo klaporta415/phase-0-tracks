001011afe4d07f41a898f633208a4b3f11a0445c
kaley.laporta <kaley.laporta@gmail.com>
2017-03-01 15:04:57 -0600

    attempt to make get_mad_at method using index to find reinder name. unsuccessful so far

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -8,25 +8,72 @@ class Santa
 		puts "That was a good #{cookie_type}!"
 	end
 
-	def initialize(gender, ethnicity)
+	def initialize(birthname, gender, ethnicity)
 		puts "Initializing Santa instance..."
 		@gender = gender
 		@ethnicity = ethnicity
-		# @age = 0
-		# @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+		@birthname = birthname
+		@age = 0
+		@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
 	end
-end
 
-santas = []
+	def birthname
+		@birthname
+	end
 
-gender = ["GNC", "female", "trans-masculine", "agender"]
+	def gender
+		@gender
+	end
 
-ethnicity = ["black", "latinx", "white", "Brazilian"]
+	def ethnicity
+		@ethnicity
+	end
 
-gender.length.times do |x|
-	santas << Santa.new(gender[x], ethnicity[x])
+	def age
+		@age
+	end
+
+	def reindeer_ranking
+		@reindeer_ranking
+	end
+
+	def celebrate_birthday
+		age.to_i + 1
+	end
+
+	def get_mad_at(reindeer_name)
+		reindeer_ranking.map{|reindeer_name| reindeer_name(-1)}
+	end
+
 end
 
 # mall_santa = Santa.new
 # mall_santa.speak
 # mall_santa.eat_milk_and_cookies("chocolate chip")
+
+# santas = []
+
+# birthname = ["Eli", "Teresa", "Camden", "Jose"]
+
+# gender = ["GNC", "female", "trans-masculine", "agender"]
+
+# ethnicity = ["black", "latinx", "white", "Brazilian"]
+
+# gender.length.times do |x|
+# 	santas << Santa.new(birthname[x], gender[x], ethnicity[x])
+# end
+
+# puts "Here them all shout their Santa call!"
+# santas.each do |santas|
+# 	puts "#{santas.birthname} says #{santas.speak}"
+# end
+
+# santas.each do |santas|
+# 	puts "Hi, my name is #{santas.birthname} and I identify as #{santas.gender}"
+# end
+
+get_mad_at("Dancer")
+p @reindeer_ranking
+
+# to confirm that santas were initialized with gender, ethnicity pairings
+# p santas
