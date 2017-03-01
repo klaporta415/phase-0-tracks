2b221759bae1c24e801689cbe28ab88cb123f07a
kaley.laporta <kaley.laporta@gmail.com>
2017-03-01 13:37:25 -0600

    array of gender and ethnicity created and added to santa array.

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -8,11 +8,25 @@ class Santa
 		puts "That was a good #{cookie_type}!"
 	end
 
-	def initialize
+	def initialize(gender, ethnicity)
 		puts "Initializing Santa instance..."
+		@gender = gender
+		@ethnicity = ethnicity
+		# @age = 0
+		# @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
 	end
 end
 
-mall_santa = Santa.new
-mall_santa.speak
-mall_santa.eat_milk_and_cookies("chocolate chip")
+santas = []
+
+gender = ["GNC", "female", "trans-masculine", "agender"]
+
+ethnicity = ["black", "latinx", "white", "Brazilian"]
+
+gender.length.times do |x|
+	santas << Santa.new(gender[x], ethnicity[x])
+end
+
+# mall_santa = Santa.new
+# mall_santa.speak
+# mall_santa.eat_milk_and_cookies("chocolate chip")
