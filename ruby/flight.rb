feb9d00c19087b596f9888c4ca6daccd32855dd0
kaley.laporta <kaley.laporta@gmail.com>
2017-03-01 23:17:42 -0600

    Flight module added to each class

--- ruby/flight.rb
+++ ruby/flight.rb
@@ -3,9 +3,14 @@ module Flight
     puts "Taking off and ascending until reaching #{altitude} ..."
   end
 end
+# self keyword not used because module is being used as a mixin
+# define method same way you define instance method inside of class
+
 
 class Bird
+	include Flight
 end
 
 class Plane
+	include Flight
 end
