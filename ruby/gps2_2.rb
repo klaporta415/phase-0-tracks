5552efcb88600a1d45ca1c865646bacf97307234
kaley.laporta <kaley.laporta@gmail.com>
2017-03-02 20:17:54 -0600

    code updated to include refactoring from pairing session.

--- ruby/gps2_2.rb
+++ ruby/gps2_2.rb
@@ -1,44 +1,47 @@
+#dry code is good code and so if efficient code
+#replace code used often with a class
+
 # Method to create a list
 # input: string of items separated by spaces (example: "carrots apples cereal pizza")
-# steps: 
-	# split method to create array out of the input (string)
-  # [fill in any steps here]
-  # set default quantity to 1
-  # print the list to the console [can you use one of your other methods here?]
+# steps:
+   # split method to create array out of the input (string)
+ # [fill in any steps here]
+ # set default quantity to 1
+ # print the list to the console [can you use one of your other methods here?]
 # output: [what data type goes here, array or hash?]
 
 def create_list(groceries)
-	list = groceries.split(' ')
-	grocery_list = {}
-	list.each do |x|
-		grocery_list[x] = 1
-	end
-	grocery_list
-end	
+   list = groceries.split(' ')
+   grocery_list = {}
+   list.each do |item|
+       grocery_list[item] = 1
+   end
+   grocery_list
+end    
 
 p list = create_list("carrots apples cereal pizza")
 
-# Method to add an item to a list 
+# Method to add an item to a list
 # input: list, item name, and optional quantity
 # steps: access items by index
 # output: hash with keys being array items
 
-def add_item(list, item, quantity)
-	list[item] = quantity
-end
+# def add_item(list, item, quantity)
+#     list[item] = quantity
+# end
 
-add_item(list, "bananas", 3)
-p list
+# add_item(list, "bananas", 3)
+# p list
 
 # Method to remove an item from the list
 # input: item name
 # steps: loop through list to see if input matches any items
-	# delete item
+   # delete item
 # output: updated list - with item deleted
 
 def remove_item(list, item)
-	list.delete(item)
-end	
+   list.delete(item)
+end    
 
 remove_item(list, "carrots")
 p list
@@ -49,7 +52,7 @@ p list
 # output: updated quantity
 
 def update_quantity(list, item, quantity)
-	list[item] = quantity
+   list[item] = quantity
 end
 
 update_quantity(list, "pizza", 3)
@@ -57,13 +60,11 @@ p list
 
 # Method to print a list and make it look pretty
 # input: hash
-# steps: interpolation 
+# steps: interpolation
 # output: print in string format
 
 def print_list(list)
-	list.each do |item, quantity| 
-	puts "Get #{quantity} #{item}"
-	end
+   list.each do |item, quantity|
+       puts "Get #{quantity} #{item}"
+   end
 end
-
-print_list(list)
