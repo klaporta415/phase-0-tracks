2ed917ff024039e1e746500893238df190a6aca3
kaley.laporta <kaley.laporta@gmail.com>
2017-03-04 10:41:49 -0600

    required wordguesser in spec file.

--- ruby/game/word_guess_spec.rb
+++ ruby/game/word_guess_spec.rb
@@ -1,3 +1,5 @@
+require_relative 'wordguesser'
+
 describe WordGuesser do
 	let(:word) { WordGuesser.new(word_input)}
 
@@ -5,7 +7,7 @@ describe WordGuesser do
 		expect(word.get_word).to eq ("unicorn")
 	end
 
-	it "makes blank line length for user to guess" do
+	it "makes blank lines length for user to guess" do
 		expect(word.make_lines).to eq ("_ _ _ _ _ _ _")
 	end
 
