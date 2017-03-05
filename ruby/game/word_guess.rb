85af5da76c584c4732785d4bfc04f75da728be31
kaley.laporta <kaley.laporta@gmail.com>
2017-03-05 16:45:13 -0600

    more methods added to ruby file including make_lines, letter_bank as a hash, and guessing_letters method unfinished.

--- ruby/game/word_guess.rb
+++ ruby/game/word_guess.rb
@@ -1,48 +1,56 @@
 # create class for game
 # initialize game instances
 # each time user inputs word to guess, new instance created
-module WordGuesser
+class WordGuesser
 
-class PlayerOne
+	attr_accessor :word, :word_input, :mystery_word, :guesses, :letter
 
-	attr_accessor :word
-	attr_accessor :word_input
-
-	def initialize(word)
+	def new_game(word)
 		@word_length = word.length
 		@update = "_ " * word.length
-		@guesses = word.length + 2
+		@guesses = word.length + 5
 		@letters_guessed = []
 		@word = word
+		@mystery_word = mystery_word
+		@word_array = @word.split(' ')
 	end
 
 	def get_word(word_input)
-		word_input = gets.chomp
-		word = word_input
-		@word = word
+		# @word_input = gets.chomp
+		@word = word_input
+		# @word = word
 	end
 
 	def make_lines(word)
 		dashes = word.length
-		making_blanks = dashes.times('_ ')
-		puts make_blanks
-		# puts @word
+		@mystery_word = dashes.times {|word| print '_ '}
 	end
-		# @word
-end
-class PlayerTwo
   
-  def letter_bank(letters)
-    letters_guessed << letters
-  end
-  
-  def 
+  	def letter_bank(letter)
+    	letters_guessed << letter
+  	end
 
+  	def guessing_letters (letter)
+  		@word.
+  	end
 
+end 
 
+puts 'Player 1, enter a word for your opponent to guess:'
+game = WordGuesser.new
+@word = gets.chomp
 
+game.new_game(@word)
+@word
 
-# get_word(word_input)
+game.make_lines(@word)
+
+puts "Player 2, above is the myster word. 
+Guess one letter at a time. 
+You have #{game.guesses} guesses."
+
+guess = gets.chomp
+
 
 # need module for game? classes are player1 that gives word and
 	# player2 that is guessing?
--- ruby/game/word_guess_spec.rb
+++ ruby/game/word_guess_spec.rb
@@ -2,22 +2,22 @@ require_relative 'word_guess'
 # require 'rspec'
 
 describe WordGuesser do
-	let(:word) { WordGuesser.new(word) }
+	# let(:word) { WordGuesser.new(word) }
 
-	# it "takes input from user" do
-	# 	get_word('unicorn')
-	# 	expect(:word.get_word).to eq ('unicorn')
-	# end
+	it "takes input from user" do
+		get_word('unicorn')
+		expect(:word.get_word).to eq ('unicorn')
+	end
 
-	it "gets word from user upon initialization" do
-		expect(word.WordGuesser.new('unicorn')).to eq (
-			@word_length = 7
-			@guesses = 9
-			@letters_guessed = nil
-			@update = "_ _ _ _ _ _ _ "
-		)
+	# it "gets word from user upon initialization" do
+	# 	expect(word.WordGuesser.new('unicorn')).to eq (
+	# 		@word_length = 7
+	# 		@guesses = 9
+	# 		@letters_guessed = nil
+	# 		@update = "_ _ _ _ _ _ _ "
+	# 	)
 		# expect(word.get_word('unicorn')).to eq ('unicorn')
-  	end
+  	# end
 
 	it "makes blank lines length for user to guess" do
 		# make_lines('unicorn')
