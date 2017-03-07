f292c3a9ec33a8ebed8fbcabb27d3deceaed3584
kaley.laporta <kaley.laporta@gmail.com>
2017-03-07 10:59:36 -0600

    Function defined for reversing strings.

---
+++ js/explore.js
@@ -0,0 +1,16 @@
+// Define method reverse
+// input: variable that is a string
+// string is separated into letters
+// letters are rearranged (loop?)
+// letters joined back together
+// output: that string written backward/reversed
+// output stored in new variable
+
+function reverse(string) {
+	var arr = string.split;
+	var wordReveresed = [].concat(arr).reverse();
+	// var wordReveresed = concat(reverseArr);
+	return wordReveresed
+}
+
+console.log(reverse("shit"));
