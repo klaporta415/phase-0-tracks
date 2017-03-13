ba241f9fbf0269fdacbd9550b0eac926e177d1a4
kaley.laporta <kaley.laporta@gmail.com>
2017-03-12 22:57:00 -0500

    Added function to make arrays of random strings in addition to function that generates random strings. Tested in driver code.

--- js/algos.js
+++ js/algos.js
@@ -33,3 +33,31 @@ steve = {name: "Steven", age: 54}
 tamir = {name: "Tamir", age: 54}
 
 objectCompare(steve, tamir)
+
+// Function: make an array out of random strings of a given length
+// Input: number of strings that will be in the array
+// Output: array of random strings
+
+
+function randomStrings(length) {
+	var string = "";
+	var characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	for (var i = 0; i < length; i++)
+		string += characters.charAt(Math.floor(Math.random() * characters.length));
+
+	return string;
+}
+
+// Adding Math.random to the function input allows length of string to be between 1-10
+	
+function arrayMaker(length){
+	var randomArray = [];
+	for (var i = 0; i < length; i++)
+	randomArray.push(randomStrings(Math.random() * (10 - 1) + 1));
+	
+	return randomArray;
+}
+
+console.log(arrayMaker(10))
+
+console.log(longestString(arrayMaker(10)))
