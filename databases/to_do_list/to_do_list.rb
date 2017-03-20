45b3225c10f7594c2193df4dd6fc2981ad0913e1
kaley.laporta <kaley.laporta@gmail.com>
2017-03-19 19:33:48 -0500

    Creation of new database and method to name new tables in database for to do list creation.

---
+++ databases/to_do_list/to_do_list.rb
@@ -0,0 +1,28 @@
+require 'sqlite3'
+
+to_do = SQLite3::Database.new("to_do_lists.db")
+
+# Method: adds items to list - exit/quit when finished (loop)
+# Input: strings from user (gets.chomp)
+# Output: saves data to database (use array)
+
+# Method: retrieves items from list
+# Input: User wants to access list (yes/no? or which list?) 
+# Output: use SELECT * FROM list and print list
+
+# Method: deletes items from list when they are complete
+# Input: user able to select item from list (by key? by entering string?)
+# Output: Print list with item deleted - encouraging comment (you go gurl!)
+
+# Ask user to name list
+puts "What do you want to name your to do list?"
+list_name = gets.chomp
+
+create_new_list = <<-NEW 
+	CREATE TABLE IF NOT EXISTS list_name(
+		id INTEGER PRIMARY KEY,
+		task VARCHAR(255)
+	)
+NEW
+
+to_do.execute(create_new_list)
